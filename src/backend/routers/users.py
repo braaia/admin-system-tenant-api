600229327d07c494d9df5d57384a8d5e263f4b86
Wesley Nogueira <wessleynog@gmail.com>
2026-01-25 11:43:27 -0300

    fix: tenant requests

--- src/backend/routers/users.py
+++ src/backend/routers/users.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from backend.dependencies import get_db
+from backend.dependencies import get_db, get_tenant
+from backend.models.tenant import Tenant
 from backend.schemas import UsuariosInLogin, UsuariosIn, UsuariosWithToken, UsuariosOut
 from backend.services.userService import UserService
 
@@ -10,17 +11,30 @@ router = APIRouter(prefix="/auth-usuarios-adm")
 
 @router.post("/login", response_model=UsuariosWithToken, tags=["Usuarios"], name="Se autenticar em uma Conta",
              status_code=200)
-def login(loginDetails: UsuariosInLogin, db: Session = Depends(get_db)):
+def login(
+    loginDetails: UsuariosInLogin,
+    db: Session = Depends(get_db),
+    tenant: Tenant = Depends(get_tenant),
+):
     try:
-        return UserService(db).login(loginDetails)
+        token_data = UserService(db).login(loginDetails)
+        if not token_data.tenant_schema:
+            token_data.tenant_schema = tenant.schema
+        return token_data
     except Exception as error:
         print(error)
         raise error
 
 
 @router.post("/sign-up", response_model=UsuariosOut, tags=["Usuarios"], name="Cadastrar uma Conta", status_code=201)
-def registrar(registrarDetails: UsuariosIn, db: Session = Depends(get_db)):
+def registrar(
+    registrarDetails: UsuariosIn,
+    db: Session = Depends(get_db),
+    tenant: Tenant = Depends(get_tenant),
+):
     try:
+        if not registrarDetails.tenant_schema:
+            registrarDetails.tenant_schema = tenant.schema
         return UserService(db).signup(registrarDetails)
     except Exception as error:
         print(error)
